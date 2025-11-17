604c7bd4cd6e1c3d6f9af2af723a8ec9a5abd400
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2025-11-17 18:24:25 +0000

    nada nuevo

--- src/app.js
+++ src/app.js
@@ -5,31 +5,6 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-
-  const alert = document.querySelector('.alert');
-
-  console.log(alert);
-
-  alert.addEventListener('click',function(event) {
-    console.log(event);
-    if(alert.classList.contains('alert-warning')) {
-        alert.classList.remove('alert-warning');
-        alert.classList.add('alert-info');
-    } else {
-        alert.classList.remove('alert-info');
-        alert.classList.add('alert-warning');
-    }
-  })
-
-  const inputLastName = document.getElementById("lastname");
-
-  inputLastName.addEventListener('input', function(event) {
-    // console.log(event);
-    console.log(event.target.value, 'value')
-    console.log('Input Lastname')
-  });
-
+window.onload = function () {
 
 };
--- src/scripts.js
+++
@@ -1,7 +0,0 @@
-      function onClickButton() {
-        console.log('Click')
-      }
-
-      function onChangeInput() {
-        console.log('Input');
-      }
